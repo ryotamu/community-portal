6c4440032ae5c9fe29af2c8c118acc105d3573d9
ryotamu <ryotamu@users.noreply.github.com>
2020-10-03 09:32:07 +0000

    deploy: b08cb2087980e9bd966ed85d686a8ec285f68549

---
+++ _nuxt/static/1601717514/clubs/4ocxrx1az/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/clubs/4ocxrx1az", (function(a){return {data:[{item:{id:"4ocxrx1az",createdAt:a,updatedAt:"2020-10-03T09:30:25.703Z",publishedAt:a,name:"脱出ごっこ部",introduction:"\u003Cp\u003E「脱出ごっこ」というアプリゲームをオンライン上でみんなで遊んだり、プレイ画像を見せ合ってお話したりするクラブです。\u003C\u002Fp\u003E",chief:"しおんy"}}],fetch:[],mutations:void 0}}("2020-09-21T01:55:21.658Z")));
