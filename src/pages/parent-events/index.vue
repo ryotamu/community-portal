ed1b960fe8a9550876b10fbd602ede01aa5646bc
ryotamu <r3t3.5.0902@gmail.com>
2021-09-12 13:20:51 +0900

    保護者カレンダーをおこさまカレンダーと一緒にする

--- src/pages/parent-events/index.vue
+++ src/pages/parent-events/index.vue
@@ -6,16 +6,9 @@
       </nuxt-link>
     </div>
     <b-card class="mx-2">
-      <h2 class="post-title font-weight-bold mb-2">保護者イベント</h2>
+      <h2 class="post-title font-weight-bold mb-2">イベントカレンダー</h2>
       <div class="d-flex justify-content-center">
-        <iframe
-          src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=1&amp;bgcolor=%2333B679&amp;ctz=Asia%2FTokyo&amp;src=aHNldW1wN283cWdra3NzbTEzbjVyZGhoaHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23B39DDB&amp;showTitle=0&amp;showNav=0&amp;showDate=0&amp;showTz=0&amp;mode=AGENDA&amp;showCalendars=1&amp;showTabs=0&amp;showPrint=0"
-          style="border-width: 0;"
-          width="600"
-          height="400"
-          frameborder="0"
-          scrolling="no"
-        ></iframe>
+        <iframe src="https://calendar.google.com/calendar/embed?height=450&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=Y185cGFsMjZzYzRoZ3JkbjJnYm10YzNqOWdzZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showDate=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
       </div>
     </b-card>
     <div class="ml-3">
--- src/pages/parent-posts/index.vue
+++ src/pages/parent-posts/index.vue
@@ -3,7 +3,7 @@
     b-list-group.mx-2.mb-2
       nuxt-link(:to="'/parent-events/'")
         b-list-group-item.d-flex.post-title.font-weight-bold
-          | 保護者さま向けイベントカレンダーはこちら！
+          | イベントカレンダーはこちら！
           div(
             style="margin-left: auto"
           )
