4d514c6985b44500bc40e8125ea75f45e8c9d23a
ryotamu <r3t3.5.0902@gmail.com>
2022-08-13 11:56:17 +0900

    カレンダーの縦幅を広げる

--- src/pages/events/index.vue
+++ src/pages/events/index.vue
@@ -5,10 +5,17 @@
         <p>一覧 ></p>
       </nuxt-link>
     </div>
-    <b-card class="mx-2">
+    <b-card class="mx-2" style="height: 1500px;">
       <h2 class="post-title font-weight-bold mb-2">イベントカレンダー</h2>
       <div class="d-flex justify-content-center">
-        <iframe src="https://calendar.google.com/calendar/embed?height=450&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=Y185cGFsMjZzYzRoZ3JkbjJnYm10YzNqOWdzZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showDate=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA" style="border-width:0" width="600" height="400" frameborder="0" scrolling="no"></iframe>
+        <iframe
+          src="https://calendar.google.com/calendar/embed?height=450&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=Y185cGFsMjZzYzRoZ3JkbjJnYm10YzNqOWdzZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showDate=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA"
+          style="border-width: 0;"
+          width="600"
+          height="400"
+          frameborder="0"
+          scrolling="no"
+        ></iframe>
       </div>
     </b-card>
     <div class="ml-3">
@@ -18,10 +25,7 @@
     </div>
   </div>
 </template>
-
-<script lang="ts">
-</script>
-
+<script lang="ts"></script>
 <style>
 .post-title {
   color: #5f5f5f;
