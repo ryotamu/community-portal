8ca017b0d1025a1d02b2429f552d5a04fde157d4
ryotamu <r3t3.5.0902@gmail.com>
2023-02-08 19:02:49 +0900

    カレンダーの縦の長さを伸ばす

--- src/pages/parent-events/index.vue
+++ src/pages/parent-events/index.vue
@@ -9,7 +9,7 @@
       <h2 class="post-title font-weight-bold mb-2">イベントカレンダー</h2>
       <div class="d-flex justify-content-center" style="height: 1200px;">
         <iframe
-          src="https://calendar.google.com/calendar/embed?height=450&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=Y185cGFsMjZzYzRoZ3JkbjJnYm10YzNqOWdzZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showDate=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA"
+          src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=Y185cGFsMjZzYzRoZ3JkbjJnYm10YzNqOWdzZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showDate=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA"
           style="border-width: 0;"
           width="600"
           height="600"
