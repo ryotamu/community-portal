73dabc680e5a50e4d0bcc1b65832e58a69887643
ryotamu <r3t3.5.0902@gmail.com>
2023-02-09 09:52:48 +0900

    子どもカレンダーの縦幅を伸ばす

--- src/pages/events/index.vue
+++ src/pages/events/index.vue
@@ -10,12 +10,12 @@
       <p>
         ※Safariブラウザの場合カレンダーが上手く表示されない場合があります。その場合はGoogleChrome等別のブラウザでご確認ください。
       </p>
-      <div class="d-flex justify-content-center">
+      <div class="d-flex justify-content-center" style="height: 1200px;">
         <iframe
-          src="https://calendar.google.com/calendar/embed?height=450&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=Y185cGFsMjZzYzRoZ3JkbjJnYm10YzNqOWdzZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showDate=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA"
+          src="https://calendar.google.com/calendar/embed?height=900&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FTokyo&amp;src=Y185cGFsMjZzYzRoZ3JkbjJnYm10YzNqOWdzZ0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23EF6C00&amp;showTitle=0&amp;showNav=0&amp;showPrint=0&amp;showDate=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA"
           style="border-width: 0;"
           width="600"
-          height="400"
+          height="900"
           frameborder="0"
           scrolling="no"
         ></iframe>
